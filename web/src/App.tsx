7b24b638ebc9a9949538bd52cd291b2b6e3c23af
SamuelRicardoDS <samuelricardods@gmail.com>
2023-01-18 11:30:52 -0300

    feat(expo,prisma, fastify, tailwind, vite): create the server and mobile and web clients of the application

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+  <div>
+    <h1 className='bg-slate-600'>hello</h1>
+    <Habit/>
+  </div>
+  )
+}
+
+export default App
