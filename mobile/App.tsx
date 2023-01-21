9c6a54b298be4f3fc83bee887220f3a266fdc78d
SamuelRicardoDS <samuelricardods@gmail.com>
2023-01-21 12:10:03 -0300

    feat(): mobile interface

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,6 +8,7 @@ import {
   } from '@expo-google-fonts/inter'
 
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -23,22 +24,10 @@ export default function App() {
     )
   }
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Home/>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#FFFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -3,16 +3,18 @@ module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./index.html"
   ],
   theme: {
     extend: {
       colors: {
         background: '#09090A',
       },
-      gridTemplateRows: {
-        7: 'repeat(7, minmax(0, 1fr))'
-      }
+      fontFamily: {
+        regular: 'Inter_400Regular',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+        extrabold: 'Inter_800ExtraBold'
+      },
     },
   },
   plugins: [],
